66112ea782f90b56458afacdd10483eb8c0328a7
Alex Sawyer <alexsawyeryt13@gmail.com>
2022-02-22 18:36:43 +0000

    Collisions

--- Game/src/Game/Player.cpp
+++ Game/src/Game/Player.cpp
@@ -9,19 +9,21 @@
 #include <algorithm>
 
 glm::vec3 s_WorldUp(0.0f, 1.0f, 0.0f);
-glm::vec3 s_Gravity(0.0f, -1.0f, 0.0f);
 constexpr float s_SensitivityModifier = 0.02f;
+constexpr float s_Gravity = -9.81f;
+constexpr float s_TerminalVelocity = -20.0f;
+const glm::vec3 s_PlayerEyeOffset(0.0f, 1.8f, 0.0f);
 
-constexpr float s_EyeLevel = 1.6f;
-constexpr float s_PlayerWidth = 0.8f;
-constexpr float s_PlayerHeight = 1.8f;
-constexpr float s_ForeheadSize = s_PlayerHeight - s_EyeLevel;
+constexpr float s_PlayerWidth = 0.0f;
 
-constexpr glm::vec3 s_PlayerSize(0.8f, 1.8f, 0.8f);
-const glm::vec3 s_PlayerEyeOffset(0.4f, 1.6f, 4.0f);
+glm::vec3 direction(0.0f);
 
-constexpr int s_PlayerWidthMaxBlocks = 2; // Ceil(s_PlayerWidth + 1)
-constexpr int s_PlayerHeightMaxBlocks = 3; // Ceil(s_PlayerHeight + 1)
+bool PlayerPosX(Player *player);
+bool PlayerNegX(Player *player);
+bool PlayerPosZ(Player *player);
+bool PlayerNegZ(Player *player);
+bool PlayerPosY(Player *player, float delta);
+bool PlayerNegY(Player *player, float delta);
 
 void PlayerCreate(Player *player, float speed, float sensitivity)
 {
@@ -75,8 +77,8 @@ void PlayerGetInput(Player *player)
 {
 	if (core.input.IsMouseCaptured)
 	{
-		// Movement
-		glm::vec3 direction(0.0f);
+		// Reset direction
+		direction = glm::vec3(0.0f);
 
 		// Gets the direction of the player without any vertical component
 		glm::vec3 forward = player->Direction;
@@ -99,21 +101,17 @@ void PlayerGetInput(Player *player)
 
 		// Normalises direction if it isn't 0, 0, 0
 		if (direction.x == 0.0f && direction.y == 0.0f && direction.z == 0.0f)
-			player->Velocity = glm::vec3(0.0f);
+			direction = glm::vec3(0.0f);
 		else
-			player->Velocity = glm::normalize(direction);
+			direction = glm::normalize(direction);
 
 		// Handles flight
 		if (player->EnableFlight)
 		{
 			if (core.input.Keys[GLFW_KEY_SPACE] & Down)
-				player->Velocity += s_WorldUp;
+				direction += s_WorldUp;
 			if (core.input.Keys[GLFW_KEY_LEFT_SHIFT] & Down)
-				player->Velocity -= s_WorldUp;
-		}
-		else
-		{
-			player->Velocity += s_Gravity;
+				direction -= s_WorldUp;
 		}
 
 		// Rotation
@@ -124,86 +122,44 @@ void PlayerGetInput(Player *player)
 
 void PlayerUpdatePhysics(Player *player, World *world)
 {
-	if (player->Velocity.x == 0.0f && player->Velocity.y == 0.0f && player->Velocity.z == 0.0f)
-		return;
+	glm::vec3 deltaPosition(0.0f);
 
-	// Collisions dont' work at 0,0,0????
+	// Gravity
+	
+	if (player->Velocity.y > s_TerminalVelocity)
+		player->Velocity.y += s_Gravity;
 
-	// Calculate the players new position after the update
-	glm::vec3 deltaPosition = player->Velocity * (float)core.input.DeltaTime * player->Speed;
+	// Apply horizontal movement
+	
+	player->Velocity.x = direction.x * player->Speed;
+	player->Velocity.z = direction.z * player->Speed;
+	player->Velocity.y = direction.y * player->Speed;
+
+	if (!player->EnableFlight)
+	{
+		player->Velocity.y += s_Gravity;
+	}
 
 	if (!player->EnableNoclip)
 	{
-		glm::vec3 newPosition = player->Position + deltaPosition;
-
-		// Make a list of all blocks that the player could collide with
-		// TODO: Include the blocks on the way to the new point (not really necessary)
-		std::vector<AABB> aabbs;
-
-		for (int x = -2; x < s_PlayerWidthMaxBlocks + 2; x++)
-		{
-			for (int z = -2; z < s_PlayerWidthMaxBlocks + 2; z++)
-			{
-				for (int y = -2; y < s_PlayerHeightMaxBlocks + 2; y++)
-				{
-					ivec3 block;
-					block.x = RoundToLowest(player->Position.x) + x;
-					block.y = RoundToLowest(player->Position.y) + y;
-					block.z = RoundToLowest(player->Position.z) + z;
-					Collider collider = WorldGetBlock(world, block.x, block.y, block.z).Collider;
-
-					AABB blockBox;
-					switch (collider)
-					{
-					case Collider::Block:
-					{
-						blockBox.Position = {
-							float(block.x),
-							float(block.y),
-							float(block.z)
-						};
-						blockBox.Size = glm::vec3(1.0f);
-						blockBox.Position -= s_PlayerEyeOffset;
-						blockBox.Size += s_PlayerSize;
-						aabbs.push_back(blockBox);
-						break;
-					}
-					case Collider::None:
-					default:
-						break;
-					}
-				}
-			}
-		}
-
-		std::vector<std::pair<int, Collision>> collisions; // Int = index in aabbs vector
-
-		// Calculate the collision for each AABB
-		for (int i = 0; i < aabbs.size(); i++)
-		{
-			Collision collision = RayVsAABB(player->Position, deltaPosition, aabbs[i]);
-			if (collision.Collided && collision.TimeToHit <= 1.0f)
-			{
-				collisions.push_back({ i, collision }); // Push collisions that need to be resolved to the vector
-			}
-		}
-
-		//spdlog::debug("There have been {} collisions this frame", collisions.size());
-
-		// Sort collisions from closest TimeToHit to furthest
-		std::sort(collisions.begin(), collisions.end(), [](const std::pair<int, Collision> &lhs, const std::pair<int, Collision> &rhs) -> bool
-			{
-				return lhs.second.TimeToHit < rhs.second.TimeToHit;
-			});
-
-		// Resolve collisions closest to furthest
-		for (int i = 0; i < collisions.size(); i++)
-		{
-			Collision &collision = collisions[i].second;
-			deltaPosition -= collision.Normal * glm::dot(deltaPosition, collision.Normal) * (1 - collision.TimeToHit);
-		}
+		if (player->Velocity.x > 0.0f && PlayerPosX(player))
+			player->Velocity.x = 0.0f;
+		if (player->Velocity.x < 0.0f && PlayerNegX(player))
+			player->Velocity.x = 0.0f;
+		if (player->Velocity.z > 0.0f && PlayerPosZ(player))
+			player->Velocity.z = 0.0f;
+		if (player->Velocity.z < 0.0f && PlayerNegZ(player))
+			player->Velocity.z = 0.0f;
+		if (player->Velocity.y > 0.0f && PlayerPosY(player, player->Velocity.y * (float)core.input.DeltaTime))
+			player->Velocity.y = 0.0f;
+		if (player->Velocity.y < 0.0f && PlayerNegY(player, player->Velocity.y * (float)core.input.DeltaTime))
+			player->Velocity.y = 0.0f;
 	}
 
+	deltaPosition = player->Velocity * (float)core.input.DeltaTime;
+
+	// Check if player has changed chunks / block
+	
 	player->OldPosition = player->Position;
 	player->Position += deltaPosition;
 
@@ -251,4 +207,62 @@ void PlayerCalculateView(Player *player)
 	player->Up = glm::cross(player->Right, s_WorldUp);
 
 	player->View = glm::lookAt(position, position + player->Direction, s_WorldUp);
+}
+
+bool PlayerPosY(Player *player, float delta)
+{
+	return (
+		WorldGetBlock(&core.world, player->Position.x - s_PlayerWidth, player->Position.y + 2.0f + delta, player->Position.z - s_PlayerWidth).Collider == Collider::Block
+		||
+		WorldGetBlock(&core.world, player->Position.x + s_PlayerWidth, player->Position.y + 2.0f + delta, player->Position.z - s_PlayerWidth).Collider == Collider::Block
+		||
+		WorldGetBlock(&core.world, player->Position.x - s_PlayerWidth, player->Position.y + 2.0f + delta, player->Position.z + s_PlayerWidth).Collider == Collider::Block
+		||
+		WorldGetBlock(&core.world, player->Position.x + s_PlayerWidth, player->Position.y + 2.0f + delta, player->Position.z + s_PlayerWidth).Collider == Collider::Block
+		);
+}
+
+bool PlayerNegY(Player *player, float delta)
+{
+	return (
+		WorldGetBlock(&core.world, player->Position.x - s_PlayerWidth, player->Position.y + delta, player->Position.z - s_PlayerWidth).Collider == Collider::Block
+		||
+		WorldGetBlock(&core.world, player->Position.x + s_PlayerWidth, player->Position.y + delta, player->Position.z - s_PlayerWidth).Collider == Collider::Block
+		||
+		WorldGetBlock(&core.world, player->Position.x - s_PlayerWidth, player->Position.y + delta, player->Position.z + s_PlayerWidth).Collider == Collider::Block
+		||
+		WorldGetBlock(&core.world, player->Position.x + s_PlayerWidth, player->Position.y + delta, player->Position.z + s_PlayerWidth).Collider == Collider::Block
+		);
+}
+
+bool PlayerPosX(Player *player)
+{
+	return (
+		WorldGetBlock(&core.world, player->Position.x + s_PlayerWidth, player->Position.y, player->Position.z).Collider == Collider::Block ||
+		WorldGetBlock(&core.world, player->Position.x + s_PlayerWidth, player->Position.y + 1.0f, player->Position.z).Collider == Collider::Block
+		);
+}
+
+bool PlayerNegX(Player *player)
+{
+	return (
+		WorldGetBlock(&core.world, player->Position.x - s_PlayerWidth, player->Position.y, player->Position.z).Collider == Collider::Block ||
+		WorldGetBlock(&core.world, player->Position.x - s_PlayerWidth, player->Position.y + 1.0f, player->Position.z).Collider == Collider::Block
+		);
+}
+
+bool PlayerPosZ(Player *player)
+{
+	return (
+		WorldGetBlock(&core.world, player->Position.x, player->Position.y, player->Position.z + s_PlayerWidth).Collider == Collider::Block ||
+		WorldGetBlock(&core.world, player->Position.x, player->Position.y + 1.0f, player->Position.z + s_PlayerWidth).Collider == Collider::Block
+		);
+}
+
+bool PlayerNegZ(Player *player)
+{
+	return (
+		WorldGetBlock(&core.world, player->Position.x, player->Position.y, player->Position.z - s_PlayerWidth).Collider == Collider::Block ||
+		WorldGetBlock(&core.world, player->Position.x, player->Position.y + 1.0f, player->Position.z - s_PlayerWidth).Collider == Collider::Block
+		);
 }
